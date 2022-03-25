a43bb8b6b94f312f5177b40cc44fb79b11e263db
Han <hanjianwei92@163.com>
2022-03-25 15:58:17 +0800

    Windows Shell Scripts

--- capture/capture.sh
+++ capture/capture.sh
@@ -1,17 +1,10 @@
-#!/bin/bash
-__conda_setup="$('${HOME}/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "${HOME}/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "${HOME}/miniconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="${HOME}/miniconda3/bin:$PATH"
-    fi
+conda activate robot
+if [ ! $? -eq 0 ]; then
+  conda init
+  echo "Please restart script"
+  sleep 5
+  exit
 fi
-unset __conda_setup
-
-conda activate detectron2
 work_dir=$(dirname $(realpath $0))
 cd $work_dir
 echo $(python --version)
--- train/train.sh
+++ train/train.sh
@@ -1,19 +1,10 @@
-#!/bin/bash
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('${HOME}/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "${HOME}/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "${HOME}/miniconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="${HOME}/miniconda3/bin:$PATH"
-    fi
+conda activate robot
+if [ ! $? -eq 0 ]; then
+  conda init
+  echo "Please restart script"
+  sleep 5
+  exit
 fi
-unset __conda_setup
-# <<< conda initialize <<<
-conda activate detectron2
 work_dir=$(dirname $(realpath $0))
 cd $work_dir
 if [ ! -d "result/" ];then
